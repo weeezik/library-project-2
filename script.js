17d162ecc6b6bc58b239ae7f6d7ea4c218920724
weeezik <mkihanya.33@gmail.com>
2025-06-12 12:47:21 -0700

    Issue with book object creation in addBookToLibrary
    The new book object that is created is set to the
    bookObjectName variable, not the value of the variable
    bookObjectName, which is a lowercased version of the
    inputted books title.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert("Page linked!")
+function Book(title, author, pages, read) {
+  this.title = title,
+  this.author = author,
+  this.pages = pages,
+  this.read = read,
+  this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary(title, author, pages, read){
+  bookObjectName = title.replaceAll(" ", "").toLowerCase(); //remove spaces and make lowercase
+  console.log(bookObjectName);
+  bookObjectName = new Book(title, author, pages, read);
+}
+
+// harryPotter = new Book("Harry Potter", "J.K. Rowling", 345, true)
+// starWars = new Book("Star Wars", "George Lucas", 557, false)
+
+addBookToLibrary("The Hobbit", "Tolkien", 783, true)
